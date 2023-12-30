f50c5a5b35efd08c8e7a39ae0e95161dd29dc5b0
cchoijjinyoung <cgy7127@naver.com>
2023-12-31 01:22:11 +0900

    feat: 서킷 브레이커 패턴 적용 및 검색 결과 캐시 저장
    resilience4j CircuitBreaker 사용

---
+++ src/main/java/com/topy/bookreview/global/config/CustomCircuitBreakerConfig.java
@@ -0,0 +1,28 @@
+package com.topy.bookreview.global.config;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class CustomCircuitBreakerConfig {
+
+  private final CircuitBreakerRegistry circuitBreakerRegistry;
+
+  @Bean
+  public CircuitBreaker circuitBreaker() {
+    CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
+        .failureRateThreshold(50)
+        .waitDurationInOpenState(Duration.ofMillis(5000))
+        .permittedNumberOfCallsInHalfOpenState(2)
+        .slidingWindowSize(6)
+        .build();
+
+    return circuitBreakerRegistry.circuitBreaker("BOOK_SEARCH", circuitBreakerConfig);
+  }
+}
