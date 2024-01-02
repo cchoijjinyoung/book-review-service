7be23bf9c7addf80d2b4da383311a7625b677171
cchoijjinyoung <cgy7127@naver.com>
2024-01-02 20:27:30 +0900

    feat: 리뷰 작성 API 추가

--- src/main/java/com/topy/bookreview/api/service/ReviewService.java
+++ src/main/java/com/topy/bookreview/api/service/ReviewService.java
@@ -6,10 +6,15 @@ import com.topy.bookreview.api.domain.repository.MemberRepository;
 import com.topy.bookreview.api.domain.repository.ReviewRepository;
 import com.topy.bookreview.api.dto.ReviewCreateRequestDto;
 import com.topy.bookreview.api.dto.ReviewCreateResponseDto;
+import com.topy.bookreview.api.dto.ReviewReadResponseDto;
+import com.topy.bookreview.api.dto.ReviewUpdateRequestDto;
+import com.topy.bookreview.api.dto.ReviewUpdateResponseDto;
 import com.topy.bookreview.global.exception.CustomException;
 import com.topy.bookreview.global.exception.ErrorCode;
-import java.util.Optional;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,7 +26,9 @@ public class ReviewService {
   private final ReviewRepository reviewRepository;
 
   @Transactional
-  public ReviewCreateResponseDto create(String email, ReviewCreateRequestDto reviewCreateRequestDto) {
+  public ReviewCreateResponseDto saveReview(String email,
+      ReviewCreateRequestDto reviewCreateRequestDto) {
+
     Member findMember = memberRepository.findByEmail(email)
         .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
 
@@ -34,5 +41,50 @@ public class ReviewService {
 
     Review savedReview = reviewRepository.save(review);
     return new ReviewCreateResponseDto(savedReview.getId());
+  }
+
+  @Transactional(readOnly = true)
+  public ReviewReadResponseDto findReviewById(Long id) {
+    Review findReview = reviewRepository.findById(id)
+        .orElseThrow(() -> new CustomException(ErrorCode.REVIEW_NOT_FOUND));
+    return ReviewReadResponseDto.fromEntity(findReview);
+  }
+
+  @Transactional(readOnly = true)
+  public Slice<ReviewReadResponseDto> findReviewsByIsbn(String isbn, Pageable pageable) {
+    Slice<Review> reviews = reviewRepository.findByIsbn(isbn, pageable);
+    return reviews.map(ReviewReadResponseDto::fromEntity);
+  }
+
+  @Transactional
+  public ReviewUpdateResponseDto editReview(String email, Long id,
+      ReviewUpdateRequestDto reviewUpdateRequestDto) {
+
+    Member findMember = memberRepository.findByEmail(email)
+        .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+    Review findReview = reviewRepository.findById(id)
+        .orElseThrow(() -> new CustomException(ErrorCode.REVIEW_NOT_FOUND));
+
+    if (!Objects.equals(findReview.getAuthor().getId(), findMember.getId())) {
+      throw new CustomException(ErrorCode.FORBIDDEN_ACCESS);
+    }
+    findReview.update(reviewUpdateRequestDto);
+    return new ReviewUpdateResponseDto(findReview.getId());
+  }
+
+  @Transactional
+  public void deleteReview(String email, Long id) {
+    Member findMember = memberRepository.findByEmail(email)
+        .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+    Review findReview = reviewRepository.findById(id)
+        .orElseThrow(() -> new CustomException(ErrorCode.REVIEW_NOT_FOUND));
+
+    if (!Objects.equals(findReview.getAuthor().getId(), findMember.getId())) {
+      throw new CustomException(ErrorCode.FORBIDDEN_ACCESS);
+    }
+
+    reviewRepository.delete(findReview);
   }
 }
