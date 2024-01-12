2ffb2193d82e9f07903e11f3280b2f9dd71c7f16
cchoijjinyoung <cgy7127@naver.com>
2024-01-13 05:49:01 +0900

    feat: 리뷰 좋아요/취소 API 추가

--- src/main/java/com/topy/bookreview/api/domain/repository/LikeRepository.java
+++ src/main/java/com/topy/bookreview/api/domain/repository/LikeRepository.java
@@ -1,10 +1,15 @@
 package com.topy.bookreview.api.domain.repository;
 
 import com.topy.bookreview.api.domain.entity.Like;
+import com.topy.bookreview.api.domain.entity.Member;
+import com.topy.bookreview.api.domain.entity.Review;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
+
+  Optional<Like> findByMemberAndReview(Member member, Review review);
 
 }
